8d124f128b617132e46ffc5e9d0c7c1f02fd4d54
Adel <56457765+aadelafattah@users.noreply.github.com>
2021-12-16 16:18:19 +0200

    First trial AI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "./style.scss";
 
 let player = true;
-let turns = 0;
 let done = false;
-let result = "";
+let result = ["", 0];
 const buttons = document.querySelectorAll(".btn");
 const end = document.querySelector("#end");
 
@@ -12,6 +11,7 @@ let board = [
 	["", "", ""],
 	["", "", ""],
 ];
+
 function drawOnBoard() {
 	buttons.forEach((button) => {
 		let place = button.id.split("btn")[1];
@@ -23,12 +23,13 @@ function drawOnBoard() {
 }
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
-		button.innerHTML = player ? "X" : "O";
 		button.disabled = true;
 		let index = button.id.split("btn")[1];
-		check(Number(index[0]), Number(index[1]), button.innerHTML);
+		board[Number(index[0])][Number(index[1])] = player ? "X" : "O";
+		drawOnBoard();
+		check(board);
 		if (done) {
-			end.innerHTML = result;
+			end.innerHTML = result[0];
 			end.hidden = false;
 			end.addEventListener("click", () => {
 				clearGrid();
@@ -37,56 +38,49 @@ buttons.forEach((button) => {
 	});
 });
 
-function check(x, y, symbol) {
-	board[x][y] = symbol;
-	if (horizontal(x)) {
-		result = win();
-	} else if (vertical(y)) {
-		result = win();
-	} else if (diagonal(x, y)) {
-		result = win();
-	} else {
-		player = !player;
-		turns++;
-		if (turns == 9) {
-			result = draw();
-		}
+function continueOn() {
+	if (emptyPlaces(board).length == 0) {
+		draw();
 	}
+	aiTurn();
 }
 
-function horizontal(x) {
-	if (board[x][0] == board[x][1] && board[x][0] == board[x][2]) {
-		return true;
-	}
-	return false;
-}
-function vertical(y) {
-	if (board[0][y] == board[1][y] && board[0][y] == board[2][y]) {
-		return true;
-	}
-	return false;
-}
-
-function diagonal(x, y) {
-	if (board[1][1] != "") {
+function check(state) {
+	let winState = [
+		[state[0][0], state[0][1], state[0][2]],
+		[state[1][0], state[1][1], state[1][2]],
+		[state[2][0], state[2][1], state[2][2]],
+		[state[0][0], state[1][0], state[2][0]],
+		[state[0][1], state[1][1], state[2][1]],
+		[state[0][2], state[1][2], state[2][2]],
+		[state[0][0], state[1][1], state[2][2]],
+		[state[2][0], state[1][1], state[0][2]],
+	];
+	for (let i = 0; i < winState.length; i++) {
 		if (
-			(board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-			(board[0][2] == board[1][1] && board[1][1] == board[2][0])
+			winState[i][0] == winState[i][1] &&
+			winState[i][1] == winState[i][1] &&
+			winState[i][0] == (player ? "X" : "O")
 		) {
-			return true;
+			win();
+			return winState[i];
 		}
 	}
-	return false;
+	continueOn();
 }
 
 function win() {
 	done = true;
-	return `${player ? "X" : "O"} WON`;
+	result[0] = `${player ? "X" : "O"} WON`;
+	result[1] = player ? 1 : -1;
+	return;
 }
 
 function draw() {
 	done = true;
-	return `DRAW`;
+	result[0] = `DRAW`;
+	result[1] = 0;
+	return;
 }
 
 function clearGrid() {
@@ -100,8 +94,69 @@ function clearGrid() {
 		button.innerHTML = "";
 	});
 	player = true;
-	turns = 0;
 	done = false;
-	result = "";
+	result[0] = "";
+	result[1] = 0;
 	end.hidden = true;
+}
+
+function emptyPlaces(state) {
+	let places = [];
+	for (let x = 0; x < 3; x++) {
+		for (let y = 0; y < 3; y++) {
+			if (state[x][y] == "") {
+				places.push([x, y]);
+			}
+		}
+	}
+	return places;
+}
+
+function aiTurn() {
+	let x, y;
+	let places = emptyPlaces(board);
+	if (places.length == 9) {
+		x = Math.trunc(Math.random() * 3);
+		y = Math.trunc(Math.random() * 3);
+	} else {
+		let previousResult = result;
+		let previousDone = done;
+		move = minimax(board, places.length, false);
+		x = move[0];
+		y = move[1];
+		result = previousResult;
+		done = previousDone;
+	}
+	board[Number(x)][Number(y)] = player ? "X" : "O";
+	check(board);
+	drawOnBoard();
+}
+
+function minimax(state, depth, player) {
+	let best;
+	best = player ? [-1, -1, 10] : [-1, -1, -10];
+	if (depth == 0) {
+		check(state);
+		return [-1, -1, result[1]];
+	}
+	emptyPlaces(state).forEach((place) => {
+		let x = Number(place[0]);
+		let y = Number(place[1]);
+		state[x][y] = player ? "X" : "O";
+		let score = minimax(state, depth - 1, !player);
+		state[x][y] = "";
+		score[0] = x;
+		score[0] = y;
+
+		if (!player) {
+			if (score[2] > best[2]) {
+				best = score;
+			} else {
+				if (score[2] < best[2]) {
+					best = score;
+				}
+			}
+		}
+	});
+	return best;
 }
